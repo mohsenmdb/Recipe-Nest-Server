c5734bd620ac6e7955805057ea71926eeb23502d
mohsenmdb <mohsen.mdb@gmail.com>
2025-12-25 12:56:47 +0330

    reture profile complete

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, UseGuards, Request } from '@nestjs/common';
+import { Controller, Get, UseGuards, Request, HttpException } from '@nestjs/common';
 import { UserService } from './user.service';
 import { JwtAuthGuard } from 'src/auth/jwt/jwt-auth.guard';
 import UserGuards from './dto/userGuards';
@@ -9,9 +9,11 @@ export class UserController {
 
   @Get()
   @UseGuards(JwtAuthGuard)
-  findOne(@Request() req) {
-    const user: UserGuards = req.user;
-    return user
-    // return this.userService.findOneById(2);
+  async profile(@Request() req: UserGuards) {
+    const user = await this.userService.findOneById(req.id);
+    if (!user) {
+      throw new HttpException('User not found', 404);
+    }
+    return {user: user};
   }
 }
